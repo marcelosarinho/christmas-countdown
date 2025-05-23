8d6a2c0594d7ed0c75b179855ecc3a76fdd717b8
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-05-22 22:03:06 -0300

    feat: function to calculate days to christmas

---
+++ src/script.js
@@ -0,0 +1,16 @@
+function calculateDays() {
+  const today = new Date();
+  const christmas = new Date(`December 25, ${today.getFullYear()}`);
+  const dayInMilliseconds = 24 * 60 * 60 * 1000;
+
+  if (today > christmas) {
+    christmas.setFullYear(christmas.getFullYear() + 1);
+  }
+
+  return Math.ceil((christmas - today) / dayInMilliseconds);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const days = calculateDays();
+  console.log(days);
+});
