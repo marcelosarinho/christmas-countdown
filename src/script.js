f3477cd456f2e9bc97a54dadc8fc690619ef48b3
Marcelo Sarinho <marceloactsarinho@gmail.com>
2025-05-26 10:54:17 -0300

    feat: lights function

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+let isRedAndGreenLightOn = true;
+
 function calculateDays() {
   const today = new Date();
   const christmas = new Date(`December 25, ${today.getFullYear()}`);
@@ -10,8 +12,37 @@ function calculateDays() {
   return Math.ceil((christmas - today) / dayInMilliseconds);
 }
 
+function toggleLights() {
+  isRedAndGreenLightOn = !isRedAndGreenLightOn;
+
+  const lights = {
+    redLight: document.getElementById("red-light"),
+    greenLight: document.getElementById("green-light"),
+    blueLight: document.getElementById("blue-light"),
+    yellowLight: document.getElementById("yellow-light"),
+  };
+
+  if (isRedAndGreenLightOn) {
+    lights.redLight.classList.replace("bg-red-900", "bg-red-500");
+    lights.greenLight.classList.replace("bg-green-900", "bg-green-500");
+    lights.blueLight.classList.replace("bg-blue-500", "bg-blue-900");
+    lights.yellowLight.classList.replace("bg-yellow-500", "bg-yellow-900");
+
+    return;
+  }
+
+  lights.blueLight.classList.replace("bg-blue-900", "bg-blue-500");
+  lights.yellowLight.classList.replace("bg-yellow-900", "bg-yellow-500");
+  lights.redLight.classList.replace("bg-red-500", "bg-red-900");
+  lights.greenLight.classList.replace("bg-green-500", "bg-green-900");
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   const days = calculateDays();
 
   document.getElementById("days").textContent = days;
 });
+
+setInterval(() => {
+  toggleLights();
+}, 1000);
